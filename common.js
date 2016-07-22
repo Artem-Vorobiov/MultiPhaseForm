da1f54b6bcf62456c5fa7b443ac99ced99d672d4
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-07-22 20:38:06 +0300

    progress

--- common.js
+++ common.js
@@ -10,6 +10,8 @@ function processPhase1() {
     if (fname.length > 2 && lname.length > 2) {
         _('phase1').style.display = "none";
         _('phase2').style.display = "block";
+        _('progressBar').value = 33;
+        _('status').innerHTML = "Phase 2 of 3"
     } else {
         alert("Please fill in the fields");
     }    
@@ -20,6 +22,8 @@ function processPhase2() {
     if (gender.length > 0){
         _('phase2').style.display = "none";
         _('phase3').style.display = "block";
+        _('progressBar').value = 66;
+        _('status').innerHTML = "Phase 3 of 3"
     } else {
         alert("Please choose your gender");
     }
@@ -30,7 +34,26 @@ function processPhase3() {
     if (country.length > 0) {
         _('phase3').style.display = 'none';
         _('show_all_data').style.display = "block";
+        _('progressBar').value = 100;
+        _('status').innerHTML = "Phase 2 of 3"
+        
+// LOOK HERE VERY USEFULL !!!!
+        
+        
+        _('display_fname').innerHTML = fname;
+        _('display_lname').innerHTML = lname;
+        _('display_country').innerHTML = country;
+        _('display_gender').innerHTML = gender;
     } else {
         alert('Please choose your Country');
     }
+}
+
+// INTERESTING
+
+function submitForm() {
+    _("multiphase").method = 'post';
+    _("multiphase").action = 'my_parser.php';
+    _("multiphase").submit();
+    
 }
